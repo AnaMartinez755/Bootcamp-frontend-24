cb95ba4c0790faeb9dead7f8daa1332a7b83d6cf
AnaMartinez775 <martinezcarballoana@gmail.com>
2024-03-12 18:49:37 -0400

    final commit

--- ecercise-1.js
+++ ecercise-1.js
@@ -1,21 +1,9 @@
+// Individual seat
 class Seat {
-  constructor(row, seat) {
+  constructor(row, seatsInRow, available) {
     this.row = row;
-    this.seat = seat;
-    this.available = true;
-  }
-
-  reserve() {
-    if (this.available) {
-      console.log(
-        `Reservation succesfull: row: ${this.row} - seat: ${this.seat}`
-      );
-      this.available = false;
-    } else {
-      console.log(
-        `Reservation failed, row: ${this.row} - seat: ${this.seat} is taken`
-      );
-    }
+    this.seatsInRow = seatsInRow;
+    this.available = available;
   }
 }
 
@@ -30,7 +18,7 @@ class Cinema {
     const seats = [];
     for (let i = 1; i <= this.rows; i++) {
       for (let j = 1; j <= this.seatsInRow; j++) {
-        seats.push(new Seat(i, j));
+        seats.push(new Seat(i, j, true));
       }
     }
     return seats;
@@ -40,11 +28,27 @@ class Cinema {
     console.log("Available seats:");
     this.seats.forEach((element) => {
       if (element.available) {
-        console.log(`Seat: ${element.row}-${element.seat}`);
+        console.log(`Seat: ${element.row}-${element.seatsInRow} available`);
+      } else {
+        console.log(`Seat: ${element.row}-${element.seatsInRow} reserved`);
       }
     });
+  }
+  reserve(row, seatNumber) {
+    console.log("Reserved:");
+    const seatIndex = this.seats.findIndex((element) => {
+      return element.row === row && element.seatsInRow === seatNumber;
+    });
+    if (seatIndex !== -1 && this.seats[seatIndex].available) {
+      this.seats[seatIndex].available = false;
+      console.log(
+        `Seat: ${this.seats[seatIndex].row}-${this.seats[seatIndex].seatsInRow} reserved`
+      );
+    }
   }
 }
 
 const cinema = new Cinema(2, 2);
+cinema.showSeatStatus();
+cinema.reserve(1, 1);
 cinema.showSeatStatus();
