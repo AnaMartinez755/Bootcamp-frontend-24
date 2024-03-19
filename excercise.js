8396e4894adb67ff3b67f20e7329220a175db58a
AnaMartinez775 <martinezcarballoana@gmail.com>
2024-03-19 18:04:59 -0400

    clean commit

--- excercise.js
+++
@@ -1,7 +0,0 @@
-function delay(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-delay(3000).then(() => console.log("Se ejecuta después de 3 segundos"));
--- palindromo.js
+++
@@ -1,9 +0,0 @@
-function Palindromo(palabra) {
-  palabra = palabra.toLowerCase().replace(/[^a-z]/g, "");
-
-  let palabraInvertida = palabra.split("").reverse().join("");
-
-  return palabra === palabraInvertida;
-}
-
-console.log(Palindromo("Anita lava la tina"));
