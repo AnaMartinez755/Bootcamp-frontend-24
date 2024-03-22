b1aa99be8b97e385c18f35a020af25855ca2be25
AnaMartinez775 <martinezcarballoana@gmail.com>
2024-03-22 09:31:20 -0400

    second commit

--- webComponent.js
+++ webComponent.js
@@ -1,18 +1,52 @@
-class MyTag extends HTMLElement {
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+  .div3 {
+    width: 30px;
+    height: 30px;
+  }
+  .main-div {
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    background-color: pink;
+    width:100%;
+    height:100vh;
+  }
+  .card-container{
+    border:solid;
+    border-color: black;
+    display:flex;
+    flex-direction:column;
+  }
+  .div1{
+    background-color: white;
+  }
+  .div2{
+    padding:10px;
+    background-color: black;
+  }
+</style>
+<div class="main-div">
+  <div class = "card-container">  
+    <div class="div1">
+      <h1>JalaSoft</h1>
+    </div>
+    <div class="div2">
+      <img src="./imiage.jpg" width="300px" height="300px">
+  </div>
+</div>
+
+
+</div>
+`;
+
+class CardItem extends HTMLElement {
   constructor() {
     super();
-    this.shadowDom = this.attachShadow({ mode: "open" });
-  }
-  connectedCallback() {
-    this.mapComponentAttributes();
-    this.render();
-    this.initComponent();
-  }
-  mapComponentAttributes() {
-    const titleAttribute = this.getAttribute("title");
-    if (titleAttribute) {
-      // Do something with the title attribute
-    }
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(template.content.cloneNode(true));
   }
 }
-export default MyTag;
+
+customElements.define("card-item", CardItem);
